9672beb74880ed6989d964208fcea9054b88ab51
Mariia <whitesheepa@gmail.com>
2021-07-24 00:27:18 +0300

    colors

--- src/Components/Collapse.js
+++ src/Components/Collapse.js
@@ -4,18 +4,27 @@ import PropTypes from "prop-types";
 export default function Collapse(props) {
   return (
     <div>
-      <span className="btn" >
-        {props.isExpanded ? 
-        <div className = "show">{props.children}</div> : <div className = "hide"></div>}
+      <p className="show" style={{backgroundColor:"#b0d17d"}}>
+        Мы жили в деревне, перед окном у нас был луг, весь золотой от множества
+        цветущих одуванчиков. Это было очень красиво. Все говорили: «Очень
+        красиво! Луг золотой». Однажды я рано встал удить рыбу и заметил, что
+        луг был не золотой, а зеленый
+      </p>
+      <span className="btn">
         {props.isExpanded ? (
-          <a 
+          <div className="show">{props.children}</div>
+        ) : (
+          <div className="hide"></div>
+        )}
+        {props.isExpanded ? (
+          <a
             style={{ color: "red", fontSize: "1.8rem" }}
             onClick={() => props.onExpandedChange()}
           >
             {props.expandedLabel}
           </a>
         ) : (
-          <a 
+          <a
             style={{ color: "green", fontSize: "1.8rem" }}
             onClick={() => props.onExpandedChange()}
           >
